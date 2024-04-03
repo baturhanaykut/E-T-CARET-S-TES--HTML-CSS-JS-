774e9b274f1bad843d6299bf2cadd438eeb197e2
Baturhan Aykut <baturhanaykut@gmail.com>
2024-04-04 01:25:52 +0300

    JavaScript ile Urun Detayı Islemlerı yapıldı

---
+++ js/single-product/colors.js
@@ -0,0 +1,14 @@
+function colorsFunc() {
+    const colorsDOM = document.querySelectorAll(".color-wrapper");
+
+    colorsDOM.forEach((color) => {
+        color.addEventListener("click", function () {
+            colorsDOM.forEach(color => {
+                color.classList.remove("active");
+            })
+            color.classList.add("active");
+        })
+    })
+}
+
+export default colorsFunc();
